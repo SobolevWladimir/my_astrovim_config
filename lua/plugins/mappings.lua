5de784f3a77440b1391a02e42c8b12fcd9d81e6c
Владимир? <Sobolev.Wladim@yandex.ru>
2024-10-04 08:56:59 +0300

    Добавил hop

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -35,25 +35,4 @@ return {
       },
     },
   },
-  {
-    "AstroNvim/astrolsp",
-    ---@type AstroLSPOpts
-    -- opts = {
-    --   mappings = {
-    --     n = {
-    --       -- this mapping will only be set in buffers with an LSP attached
-    --       K = {
-    --         function() vim.lsp.buf.hover() end,
-    --         desc = "Hover symbol details",
-    --       },
-    --       -- condition for only server with declaration capabilities
-    --       gD = {
-    --         function() vim.lsp.buf.declaration() end,
-    --         desc = "Declaration of current symbol",
-    --         cond = "textDocument/declaration",
-    --       },
-    --     },
-    --   },
-    -- },
-  },
 }
