67118dc1ca1bc99f895fa4da2c56dbfcffcd221a
Vladimir <Sobolev.Wladim@yandex.ru>
2024-12-18 11:32:39 +0300

    Добавил удаление по нужную строку

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -22,6 +22,28 @@ return {
           ["f"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true}) end, desc = "Hop hint words" },
           ["F"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true}) end, desc = "Hop hint words" },
         },
+        l = {
+          ["s"] = { function() require("hop").hint_words { extend_visual = true } end, desc = "Hop hint words" },
+          ["<S-s>"] = {
+            function() require("hop").hint_lines { extend_visual = true } end,
+            desc = "Hop hint lines",
+          },
+          ["t"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end, desc = "Hop hint words" },
+          ["T"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end, desc = "Hop hint words" },
+          ["f"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true}) end, desc = "Hop hint words" },
+          ["F"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true}) end, desc = "Hop hint words" },
+        },
+        o = {
+          ["s"] = { function() require("hop").hint_words { extend_visual = true } end, desc = "Hop hint words" },
+          ["<S-s>"] = {
+            function() require("hop").hint_lines { extend_visual = true } end,
+            desc = "Hop hint lines",
+          },
+          ["t"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end, desc = "Hop hint words" },
+          ["T"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end, desc = "Hop hint words" },
+          ["f"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true}) end, desc = "Hop hint words" },
+          ["F"] = { function() require("hop").hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true}) end, desc = "Hop hint words" },
+        },
       },
     },
   },
